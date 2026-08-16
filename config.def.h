730bc9f0ddf13ba9d72fd36adb8fa03f90620058
tomclay1x1 <raghunandanmajhi901@gmail.com>
2026-08-16 20:02:10 +0530

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -10,7 +10,6 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const char buttonbar[] = " ";
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
