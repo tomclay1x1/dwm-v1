0637fab393b55288f18837e4f466be5a3b6f552b
tomclay1x1 <raghunandanmajhi901@gmail.com>
2026-08-17 20:51:51 +0530

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -10,7 +10,10 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[] = { 
+    "FreeMono:size=14:antialias=true:autohint=true", 
+    "JetBrainsMono Nerd Font:size=14:antialias=true:autohint=true" 
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
