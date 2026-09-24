fa94108792bd1c47f1cae05e40d9d6cbac07e875
tomclay1x1 <raghunandanmajhi901@gmail.com>
2026-09-24 10:38:59 +0530

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -13,10 +13,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int vertpad            = 7;       /* vertical padding of bar */
 static const int sidepad            = 7;       /* horizontal padding of bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[] = { 
-    "FreeMono:size=13:antialias=true:autohint=true", 
-    "JetBrainsMono Nerd Font:size=13:antialias=true:autohint=true" 
-};
+static const char font[] = "FreeMono, JetBrainsMono Nerd Font 13";
+
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#191818";
 static const char col_gray2[]       = "#444444";
