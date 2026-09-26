7e2cfd421da842ccd8cd5cf44226614e83bfcbd4
tomclay1x1 <raghunandanmajhi901@gmail.com>
2026-09-26 23:56:22 +0530

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char col_gray1[]       = "#191818";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#6b767a";
+static const char col_cyan[]        = "#bac0c2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
